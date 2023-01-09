34520d824390575f3761feea7a3a649de8705070
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-01-09 13:37:56 +0800

    add basic methods

--- bin/dart_hamming_code_error_correction.dart
+++ bin/dart_hamming_code_error_correction.dart
@@ -1,1 +1,13 @@
-void main(List<String> arguments) {}
+import 'package:dart_hamming_code_error_correction/dart_hamming_code_error_correction.dart';
+
+void main(List<String> arguments) {
+  final String sampleData = '10001010011101100';
+  final String reduceBinary = reduce(getBits(sampleData));
+
+  print(reduceBinary);
+
+  final String result = getCorrectData(sampleData, reduceBinary);
+  final String reduceBinary2 = reduce(getBits(result));
+
+  print(reduceBinary2);
+}
--- lib/dart_hamming_code_error_correction.dart
+++ lib/dart_hamming_code_error_correction.dart
@@ -1,1 +1,30 @@
+import 'dart:math' as math;
 
+List<int> getBits(String data) {
+  final List<int> nonZeroIndices = <int>[];
+
+  for (int index = 0; index < data.length; index++) {
+    if (String.fromCharCode(data.codeUnitAt(index)) == '1') {
+      nonZeroIndices.add(index);
+    }
+  }
+
+  return nonZeroIndices;
+}
+
+String reduce(List<int> data) =>
+    data.reduce((value, element) => value ^ element).toRadixString(2);
+
+String getCorrectData(String data, String correction) {
+  final List<int> dataUnits = data.codeUnits.toList();
+
+  for (var index = 0; index < correction.length; index++) {
+    if (String.fromCharCode(dataUnits[index]) == '1') {
+      final int currectionIndex = math.pow(index, 2).toInt();
+
+      dataUnits[currectionIndex] = dataUnits[currectionIndex] == 49 ? 48 : 49;
+    }
+  }
+
+  return String.fromCharCodes(dataUnits);
+}
