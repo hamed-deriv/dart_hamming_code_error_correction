ae31a24d838886c7d1266053d2a8a63fe111f562
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-01-10 18:49:29 +0800

    add tests

--- test/dart_hamming_code_error_correction_test.dart
+++ test/dart_hamming_code_error_correction_test.dart
@@ -1,5 +1,23 @@
+import 'package:dart_hamming_code_error_correction/dart_hamming_code_error_correction.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Hamming Code Test =>', () {});
+  test('getNonZeroIndices', () {
+    expect(getNonZeroIndices('0000'), equals(<int>[]));
+    expect(getNonZeroIndices('1001'), equals(<int>[0, 3]));
+    expect(getNonZeroIndices('1101'), equals(<int>[0, 1, 3]));
+  });
+
+  test('reduce', () {
+    expect(reduce(<int>[0, 1, 0, 1]), equals('0'));
+    expect(reduce(<int>[1, 1, 1, 0]), equals('1'));
+    expect(reduce(<int>[0, 1, 0, 0]), equals('1'));
+  });
+
+  test('getCorrectData', () {
+    expect(
+      getCorrectData('10001010011101100', '1001'),
+      equals('11001010001101100'),
+    );
+  });
 }
